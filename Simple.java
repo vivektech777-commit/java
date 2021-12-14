65e66084c061556edd2fcfbdb384d150d7060135
root <vivektech777@gmail.com>
2021-12-14 21:50:02 +0000

    second

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java first");  
+     System.out.println("Hello Java first i love india");  
     }  
 }  
